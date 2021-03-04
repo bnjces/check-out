e316996c0593e23dbff2b0d4a7949f3e40cb1c90
bnjces <bnjces@gmail.com>
2021-03-03 21:01:59 -0500

    Globalized variables for use throughout JS file.

--- js/main.js
+++ js/main.js
@@ -13,26 +13,26 @@ function init() {
 
 function updateCart() {
     // zero out running totals
-    var extendPrice = 0.0;
+    var extendedPrice = 0.0;
     var tax = 0.0;
     // find each <tr> except for first and last
-    let rows = document.getElementsByTagName("tr");     // grabs all the table rows, returns node list
+    var rows = document.getElementsByTagName("tr");     // grabs all the table rows, returns node list
     console.log(rows);                                  // check to see if the rows have been grabbed
     for(let i = 1; i < rows.length - 1; i++){           // for loop on the rows, ignoring the first and last rows (first row is titles, last row is the total)
         console.log(rows[i]);                           // shows what the rows are, to check if the for loop was done correctly (it was!)
-        let childNodes = rows[i].childNodes             // access child nodes in row (individual data points, price etc)
+        var childNodes = rows[i].childNodes             // access child nodes in row (individual data points, price etc)
         console.log(childNodes);                        // check child nodes
          /*for(let j = 0; j < childNodes.length; j++){    // child nodes location finder
             console.log(childNodes[j].innerText);       // check text in child nodes to ascertain precise location of price, quantity, etc. 
          } */
-        let itemPrice = parseFloat(childNodes[2].innerText.substring(1));        // grab the price, Math function takes string and makes it into number, start at 1 (to avoid the $)
-        let qty = parseInt(childNodes[3].innerText);                            // grab the quantity of items
-        let extendedPrice = itemPrice * qty;
+        var itemPrice = parseFloat(childNodes[2].innerText.substring(1));        // grab the price, Math function takes string and makes it into number, start at 1 (to avoid the $)
+        var qty = parseInt(childNodes[3].innerText);                            // grab the quantity of items
+        var extendedPrice = itemPrice * qty;
         childNodes[5].innerText = formatter.format(extendedPrice);       // set the extended price in its place in the table row
         
             // to do  assume tax rate of 8% add tax value to row and running total tax: use switch
-        let taxable = childNodes[6].innerText;
-        let salesTax = 0.08;        
+        var taxable = childNodes[6].innerText;
+        var salesTax = 0.08;        
         switch (taxable) {
             case "Y":
                 childNodes[7].innerText = formatter.format(extendedPrice * salesTax);
@@ -42,5 +42,8 @@ function updateCart() {
                 break;
         }
     }
+    let totalsRow = rows[i].length;         // gives location of the totals row, the last row regardless of amt of rows
+
     // to do  place total price in <td> last row, total tax in <td> 
+
 }   
